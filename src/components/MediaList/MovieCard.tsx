f6177a5f32c8cf2f7defb8f3c76b53db1ae691d9
lhnhidev <lhnhi420@gmail.com>
2026-04-15 23:12:33 +0700

    feat: implement dynamic MovieCard rendering with media data fetching

--- src/components/MediaList/MovieCard.tsx
+++ src/components/MediaList/MovieCard.tsx
@@ -1,22 +1,52 @@
 import CircularProgressBar from "./CircularProgress"
 
-const MovieCard = () => {
+type MovieCard = {
+  title: string | undefined
+  posterPath: string | undefined
+  point: number | undefined
+  releaseDay: string | undefined
+  isTvShow: boolean
+}
+
+const MovieCard = ({
+  title,
+  posterPath,
+  point,
+  releaseDay,
+  isTvShow
+}: MovieCard) => {
   return (
     <div className="rounded-lg border border-white">
-      <div>
+      <div className="relative">
+        {isTvShow && (
+          <div className="absolute top-2 right-2 rounded-md bg-white p-1 text-black opacity-70">
+            Tv show
+          </div>
+        )}
         <img
           className="rounded-t-lg"
-          src="https://media.themoviedb.org/t/p/w600_and_h900_face/in1R2dDc421JxsoRWaIIAqVI2KE.jpg"
+          src={`https://image.tmdb.org/t/p/original/${posterPath}`}
           alt="thumbnail_movie"
         />
       </div>
 
       <div className="relative p-5">
         <div className="absolute -top-5">
-          <CircularProgressBar />
+          <CircularProgressBar
+            percent={point}
+            strokeColor={
+              point === undefined
+                ? "green"
+                : point >= 80
+                  ? "green"
+                  : point >= 60
+                    ? "yellow"
+                    : "red"
+            }
+          />
         </div>
-        <p className="mt-6 text-lg font-bold">The boys season 5</p>
-        <p>2026-08-04</p>
+        <p className="mt-6 text-lg font-bold">{title}</p>
+        <p>{releaseDay}</p>
       </div>
     </div>
   )
--- src/components/MediaList/index.tsx
+++ src/components/MediaList/index.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react"
 import MovieCard from "./MovieCard"
 
 const MediaList = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [mediaList, setMediaList] = useState<Array<Record<string, any>>>([])
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/trending/all/day?language=en-US", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDc2OGExMTZkMTA2ZDgwNWI0NjdiYzVmYTcwN2UwZSIsIm5iZiI6MTc0NTg1NzYzOC41NDMsInN1YiI6IjY4MGZhYzY2YTkwYWNhZjZlZWVhZTBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qfLXEhFU67ZpVdb4mPiYW0PWFZhUopzjGpeGWATHSOI"
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMediaList(data.results.slice(0, 12))
+      })
+  }, [])
+
   return (
     <div className="bg-slate-950 px-8 py-10 text-white">
       <div className="pb-10">
@@ -16,12 +35,18 @@ const MediaList = () => {
       </div>
 
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {mediaList.map((media) => {
+          return (
+            <MovieCard
+              key={media?.id}
+              title={media?.title || media?.name}
+              posterPath={media?.poster_path}
+              point={Math.round((media?.vote_average ?? 0) * 10)}
+              releaseDay={media?.release_date || media?.first_air_date}
+              isTvShow={media?.media_type === "tv"}
+            />
+          )
+        })}
       </div>
     </div>
   )
