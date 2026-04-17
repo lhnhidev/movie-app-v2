34b1fa3579635c8ea47674b6eec202d81ebd6565
lhnhidev <lhnhi420@gmail.com>
2026-04-17 09:43:35 +0700

    feat: implement dynamic movie detail fetching in Banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,7 +1,47 @@
 import { FaPlay } from "react-icons/fa"
 import CircularProgressBar from "../CircularProgress"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+
+type FilmType = {
+  id: number
+  title: string
+  overview: string
+  backdrop_path: string
+  vote_average: number
+  poster_path: string
+  release_date: string
+  genres: Array<{ id: number; name: string }>
+}
 
 const Banner = () => {
+  const { id } = useParams()
+
+  const [film, setFilm] = useState<FilmType>({} as FilmType)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetch(`https://api.themoviedb.org/3/movie/${id}`, {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDc2OGExMTZkMTA2ZDgwNWI0NjdiYzVmYTcwN2UwZSIsIm5iZiI6MTc0NTg1NzYzOC41NDMsInN1YiI6IjY4MGZhYzY2YTkwYWNhZjZlZWVhZTBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qfLXEhFU67ZpVdb4mPiYW0PWFZhUopzjGpeGWATHSOI"
+          }
+        })
+
+        const data = await result.json()
+        console.log(data)
+        setFilm(data)
+      } catch (error) {
+        throw new Error((error as Error).message)
+      }
+    }
+
+    fetchData()
+  }, [id])
+
   return (
     <div className="relative overflow-hidden">
       <img
@@ -20,20 +60,35 @@ const Banner = () => {
 
         <div className="flex-2">
           <p className="text-lg font-bold max-[600px]:mb-5 min-[960px]:text-3xl">
-            Title film
+            {film?.title}
           </p>
 
           <div className="min-[960px]:text-md mt-1 mb-1 hidden items-center gap-5 text-sm min-[600px]:flex min-[960px]:mt-3 min-[960px]:mb-4">
             <div className="inline-block border border-gray-400 p-2 text-gray-400">
               G
             </div>
-            <div>2024-07-11</div>
-            <div>Sci-Fi & Fantasy, Action & Adventure</div>
+            <div>{film?.release_date}</div>
+            <div>
+              {film?.genres?.map((genre, index) => {
+                let symbol = ", "
+                if (index + 1 === film?.genres?.length) {
+                  symbol = ""
+                }
+                return (
+                  <span key={genre?.id || crypto.randomUUID()}>
+                    {genre?.name + symbol}
+                  </span>
+                )
+              })}
+            </div>
           </div>
 
           <div className="min-[960px]:text-md my-3 hidden gap-4 text-sm min-[600px]:flex min-[960px]:my-6 min-[960px]:gap-10">
             <div className="flex items-center gap-3">
-              <CircularProgressBar percent={84} strokeColor="green" />
+              <CircularProgressBar
+                percent={Math.round(film?.vote_average * 10)}
+                strokeColor="green"
+              />
               <p className="text-sm min-[960px]:text-xl">Rating</p>
             </div>
             <button className="flex items-center gap-2 text-sm min-[960px]:text-xl">
@@ -46,12 +101,7 @@ const Banner = () => {
             <p className="text-md mb-2 font-bold min-[960px]:text-xl">
               Overview
             </p>
-            <p className="min-[960px]:text-md text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              quas corporis velit ullam sed et, iste, numquam officia dolores
-              distinctio accusantium maiores consequuntur itaque iure nesciunt,
-              molestiae esse ea maxime!
-            </p>
+            <p className="min-[960px]:text-md text-sm">{film?.overview}</p>
           </div>
 
           <div className="flex gap-8 min-[960px]:gap-20">
